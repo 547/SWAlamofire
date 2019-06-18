6b543af6f7d762b444d5443ebc4e8f18f96f0701
Seven Wang <seven.wang@supernovachina.com>
2019-06-18 11:35:05 +0800

    Initial Commit

---
+++ SWAlamofire/SWAlamofire.h
@@ -0,0 +1,19 @@
+//
+//  SWAlamofire.h
+//  SWAlamofire
+//
+//  Created by Supernova SanDick SSD on 2019/6/18.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWAlamofire.
+FOUNDATION_EXPORT double SWAlamofireVersionNumber;
+
+//! Project version string for SWAlamofire.
+FOUNDATION_EXPORT const unsigned char SWAlamofireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWAlamofire/PublicHeader.h>
+
+
